dc9f49f0e22b0ded57c781c201acb8ab7949f139
FelipeSouzaG <154008572+FelipeSouzaG@users.noreply.github.com>
2026-03-26 13:55:41 -0300

    Change padding to margin in logo styling

--- src/tabs/ProdutosTab.tsx
+++ src/tabs/ProdutosTab.tsx
@@ -279,7 +279,7 @@ export function ProdutosTab() {
               width: 2.4cm;
               height: auto;
               align-self: center;
-              padding: 8px, 0, 0, 8px;
+              margin: 8px, 0, 0, 8px;
             }
             .label-desc {
               font-size: 14px;
